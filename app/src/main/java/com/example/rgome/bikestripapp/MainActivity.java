044123ed6bc372cf5dcdcaaad6b7407c7b2589c3
R. Gomes Professional <r.gomes90@hotmail.com>
2018-03-22 17:33:05 +0000

    Main Page Commit

--- app/src/main/java/com/example/rgome/bikestripapp/MainActivity.java
+++ app/src/main/java/com/example/rgome/bikestripapp/MainActivity.java
@@ -6,9 +6,12 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,29 +23,82 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    private Button btnChangePassword, btnRemoveUser, changePassword, remove, signOut;
-    private TextView email;
-    private EditText oldEmail, password, newPassword;
-    private ProgressBar progressBar;
-    private FirebaseAuth auth;
+public class MainActivity extends AppCompatActivity {
+    //Variables
+    public static Spinner selectCity;
+    public static ArrayList<String> locations = new ArrayList<String>();
+    public static ArrayList<String> moreInfo = new ArrayList<String>();
+    public static ArrayList<Double> posLatitude = new ArrayList<Double>();
+    public static ArrayList<Double> posLongitude = new ArrayList<Double>();
+    Button btnStations;
+    Button btnMap;
+    Button btnAbout;
+
+    String[] cities = {"Select City...", "Brisbane", "Bruxelles-Capitale", "Namur", "Santander", "Seville", "Valence", "Amiens", "Besancon", "Cergy-Pontoise", "Creteil", "Lyon", "Marseille", "Mulhouse", "Nancy", "Nantes", "Rouen", "Toulouse", "Dublin", "Toyama", "Vilnius", "Luxembourg", "Lillestrom", "Kazan", "Goteborg", "Lund", "Stockholm", "Ljubljana"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        email = (TextView) findViewById(R.id.useremail);
+        //Get widgets
+        selectCity = (Spinner) findViewById(R.id.selectCity);
+        btnStations = (Button) findViewById(R.id.btnStations);
+        btnMap = (Button) findViewById(R.id.btnMap);
+        btnAbout = (Button) findViewById(R.id.btnAbout);
 
-        changePassword = (Button) findViewById(R.id.changePass);
+        //Set cities in the spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, cities);
+        selectCity.setAdapter(dataAdapter);
 
-        changePassword.setOnClickListener(new View.OnClickListener() {
+        //Get data from website with about the bikes (Fecth data in background)
+        selectCity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position != 0) {
+                    if(locations.size() > 0){
+                        locations.clear();
+                        moreInfo.clear();
+                        posLatitude.clear();
+                        posLongitude.clear();
+                    }
 
-                startActivity(new Intent(MainActivity.this, AccountSettings.class));
-                finish();
+             //       FetchData process = new FetchData();
+            //        process.execute();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        //Show list of bike
+        btnStations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+        //        Intent intent = new Intent(MainActivity.this, Stations.class);
+          //      startActivity(intent);
+            }
+        });
+
+        //Show map of bike locations
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            //    Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+            //    startActivity(intent);
+            }
+        });
+
+        //Show about
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             //   Intent intent = new Intent(MainActivity.this, About.class);
+            //    startActivity(intent);
             }
         });
     }
